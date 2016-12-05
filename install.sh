6f0843faec698cc2051f030c04d72b4f430a6ee2
Willy LE ROY <wlr86fr+github@gmail.com>
2016-12-05 21:33:55 +0000

    Fixes... again

--- install.sh
+++ install.sh
@@ -2,13 +2,12 @@
 
 git clone https://github.com/VundleVim/Vundle.vim.git ./bundle/Vundle.vim
 
-cd ..
-ln -s vimconfig .vim
-ln -s vimconfig/vimrc .vimrc
+ln -s ~/vimconfig ~/.vim
+ln -s ~/vimconfig/vimrc ~/.vimrc
 
 vim +PluginInstall +qall
 
-sed -i '/^"colorscheme  /s/^"//' .vim/vimrc
+sed -i '/^"colorscheme /s/^"//' ~/.vimrc
 
 echo Done.
 
