e902e92142e9db38dce0f7059a2373ddee287867
Willy LE ROY <wlr86fr+github@gmail.com>
2016-12-06 11:12:17 +0000

    A backup of existing vim files is now operated prior to installing things

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 
 git clone https://github.com/VundleVim/Vundle.vim.git ./bundle/Vundle.vim
 
+# backup any existing config
+mkdir ~/.vimbackup
+mv -f ~/.vim* ~/.vimbackup/
+
 ln -s ~/vimconfig ~/.vim
 ln -s ~/vimconfig/vimrc ~/.vimrc
 
